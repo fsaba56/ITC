20054855388dcff793292f97a93f0b64c35fe0f1
fsaba56 <fsaba56@gmail.com>
2025-03-20 12:50:02 -1200

    Update Presentation_Pyspark_Concept.py

--- Presentation_Pyspark_Concept.py
+++ Presentation_Pyspark_Concept.py
@@ -5,7 +5,6 @@ spark = SparkSession.builder.appName("FileFormatExample").getOrCreate()
 
 # Read CSV file
 df = spark.read.option("header", True).csv("hdfs:///tmp/jenkins/data.csv")
-#df = spark.read.option("header", True).csv(r"C:/Users/fsaba/OneDrive/Documents/Training_Documents/data.csv")
 
 # Save as Parquet
 df.write.mode("overwrite").parquet("data.parquet")
@@ -16,35 +15,29 @@ df.write.mode("overwrite").orc("data.orc")
 # Save as JSON
 df.write.mode("overwrite").json("data.json")
 
-# Save as Delta (requires Delta Lake)
-#df.write.format("delta").mode("overwrite").save("sample_data_delta")
-
 # Save as TXT (tab-separated values)
 df.write.mode("overwrite").option("sep", "\t").csv("data.txt")
 
 # Verify Data
+print("Original CSV Data:")
 df.show()
-
 
 # Read Parquet
 df_parquet = spark.read.parquet("data.parquet")
-print("This is parquet")
+print("\n==== This is Parquet Format ====")
 df_parquet.show()
 
 # Read ORC
 df_orc = spark.read.orc("data.orc")
-print("This is orc")
+print("\n==== This is ORC Format ====")
 df_orc.show()
 
 # Read JSON
 df_json = spark.read.json("data.json")
-print("This is json")
+print("\n==== This is JSON Format ====")
 df_json.show()
-
-# Read Delta
-#df_delta = spark.read.format("delta").load("sample_data_delta")
-#df_delta.show()
 
 # Read TXT (tab-separated)
 df_txt = spark.read.option("sep", "\t").csv("data.txt", header=True)
+print("\n==== This is TXT Format ====")
 df_txt.show()
